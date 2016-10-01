089818424cf92b45002bfbb773e2ef6d48754679
Bartek <Bartek@KRZYSZTOF-KOMPU>
2016-10-01 09:32:56 +0200

    some

---
+++ KontestRepo/KontestRepo/KontestRepo.cpp
@@ -0,0 +1,14 @@
+// KontestRepo.cpp : Defines the entry point for the console application.
+//
+
+#include <iostream>
+#include <vector>
+#include <set>
+#include <algorithm>
+
+
+int main()
+{
+    return 0;
+}
+
